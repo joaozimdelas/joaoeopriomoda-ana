8674af86375d99b9218043d782e2643f194b3fda
joaozimdelas <143225014+joaozimdelas@users.noreply.github.com>
2024-10-04 08:54:05 -0300

    Y6MU7,7P

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 let estados= ['AC','AM','AP','AL','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+let i = 0
+while (i<10 ) {console.log (i);
+
+    i= i+1 ;
+
+
+
+}

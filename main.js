41019f9072825a73132d4e41d38bdcd21701d9bb
joaozimdelas <143225014+joaozimdelas@users.noreply.github.com>
2024-10-04 09:26:19 -0300

    fnfsgn

--- main.js
+++ main.js
@@ -9,7 +9,13 @@ for (let j = 0; j < estados.length; j++) {
 console.log(j);
 console.log(estados[j]);  
 }
-
+let idade = 15
+if(idade > 17 ) {
+    console.log ('pode dirigir');
+}
+ else { 
+ console.log('ilegal dirigir');
+ }
 
 
 
